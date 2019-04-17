51f67171b6606f7d91528099462cabad6085ec3b
Naor Nagar <naor.nagar@pipl.com>
2019-04-17 07:20:52 +0300

    inital commit

---
+++ schema.js
@@ -0,0 +1,40 @@
+// Imports
+const {makeExecutableSchema} = require('graphql-tools');
+const {importSchema} = require('graphql-import');
+const typeDefs = importSchema('./schema.graphql'); // import schema definition
+const fetch = require('node-fetch');
+
+// Definitions
+const BASE_URL = 'https://api.github.com'; // should be an env var
+
+// Resolvers
+const resolvers = {
+  Query: {
+    async user(parent, args, ctx, info) {
+      if (!args.username) {
+        throw new Error('username is required')
+      }
+
+      const userInfo = await fetch(`${BASE_URL}/users/${args.username}`);
+      return userInfo.json();
+    }
+
+  },
+  User: {
+    async repositories(parent, args, ctx, info) {
+      if (!parent['repos_url']) {
+        throw new Error('missing repos_url in parent');
+      }
+      const reposInfo = await fetch(parent['repos_url']);
+      return reposInfo.json();
+    },
+  }
+
+};
+
+
+// Export Schema
+module.exports = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
